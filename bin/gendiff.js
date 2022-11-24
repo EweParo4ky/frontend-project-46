115e5710b0336c1e2aca87c6e19567d7f7ea5d8f
Ilya Sukharev <imsworkfor@gmail.com>
2022-11-24 20:38:38 +0300

    eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import genDiff from '../src/gendiff.js';
+import { Command } from "commander";
+import genDiff from "../src/gendiff.js";
 
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
+  .description("Compares two configuration files and shows a difference.")
+  .version("0.0.1", "-V, --version", "output the version number")
+  .option("-f, --format <type>", "output format")
+  .arguments("<filepath1> <filepath2>");
 
-    program.parse()
-    const [file1, file2] = program.args;
-    console.log(genDiff(file1, file2));
+program.parse();
+const [file1, file2] = program.args;
+console.log(genDiff(file1, file2));
