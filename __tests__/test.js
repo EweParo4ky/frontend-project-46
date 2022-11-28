4d7e66c9762752466b301007fcffa80b9209c3e0
Ilya Sukharev <imsworkfor@gmail.com>
2022-11-29 01:04:29 +0300

    add test

---
+++ __tests__/test.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/gendiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+console.log('sample', readFile('flatTestSample.txt').trim(), typeof readFile('flatTestSample.txt'));
+console.log('function', genDiff(getFixturePath('flatFile1.json'), getFixturePath('flatFile2.json')), typeof genDiff(getFixturePath('flatFile1.json'), getFixturePath('flatFile2.json')));
+
+test('flat JSON', () => {
+  const expectedResult = readFile('flatTestSample.txt');
+  expect(genDiff(getFixturePath('flatFile1.json'), getFixturePath('flatFile2.json'))).toEqual(expectedResult);
+});
+
+
---
+++ src/parser.js
@@ -0,0 +1,28 @@
+
+import path from "path";
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+
+const jsonParse = (json) => JSON.parse(fs.readFileSync(path.resolve(json)));
+const ymlParser = (yml) => yaml.load(fs.readFileSync(path.resolve(yml)));
+
+const getParser = (fileStr) => {
+  const extension = path.extname(fileStr);
+  if (extension === '.json') {
+    return jsonParse;
+  }
+  if(extension === '.yaml') {
+    return ymlParser;
+  }
+  else {
+    throw new Error('This file type is not supported');
+  }
+}
+
+  const parse = (file) => {
+    const fileToObject= getParser(file);
+    return fileToObject(file);
+  };
+
+  export default parse;
